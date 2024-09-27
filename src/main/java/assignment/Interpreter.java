c98c198f9ab87178f50188dba3209165dbe27a18
Brian Qu <bbq@Brians-MacBook-Pro.local>
2024-09-26 20:56:58 -0500

    added comments

--- src/main/java/assignment/Interpreter.java
+++ src/main/java/assignment/Interpreter.java
@@ -1,17 +1,9 @@
 package assignment;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Map;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.Arrays;
-import java.util.List;
-import java.lang.reflect.Method;
 /**
  * Responsible for loading critter species from text files and interpreting the
  * simple Critter language.
@@ -22,87 +14,9 @@ import java.lang.reflect.Method;
 
 
 public class Interpreter implements CritterInterpreter {
-
-
-//	public static HashMap<String, HashMap<Integer, ArrayList<Integer>>> allPaths = new HashMap<>();
-//	private boolean isValidPath(ArrayList<Integer> path, String name) {
-//		for (int i = 0; i < path.size()-1; i++) {
-//			if (!allPaths.containsKey(name) && allPaths.get(name).get(path.get(i)).contains(path.get(i+1))) {
-//				return false;
-//			}
-//		}
-//		return true;
-//	}
-//	private void createTrees() {
-//		allPaths.put("Rover", new HashMap<>());
-//		allPaths.get("Rover").put(0, new ArrayList<>());
-//		allPaths.get("Rover").get(0).add(1);
-//		allPaths.get("Rover").get(0).add(9);
-//		//if hungry
-//		allPaths.get("Rover").put(9, new ArrayList<>());
-//		allPaths.get("Rover").get(9).add(10);
-//		allPaths.get("Rover").get(9).add(12);
-//		//ifempty
-//		allPaths.get("Rover").put(1, new ArrayList<>());
-//		allPaths.get("Rover").get(1).add(2);
-//		allPaths.get("Rover").get(1).add(7);
-//		//ifrandom
-//		allPaths.get("Rover").put(2, new ArrayList<>());
-//		allPaths.get("Rover").get(2).add(5);
-//		allPaths.get("Rover").get(2).add(3);
-//
-//
-//
-//
-//		allPaths.put("Test2", new HashMap<>());
-//		allPaths.get("Test2").put(0, new ArrayList<>());
-//		allPaths.get("Test2").get(0).add(1);
-//		allPaths.get("Test2").get(0).add(6);
-//		//if hungry
-//		allPaths.get("Test2").put(1, new ArrayList<>());
-//		allPaths.get("Test2").get(1).add(2);
-//		allPaths.get("Test2").get(1).add(8);
-//		//ifempty
-//		allPaths.get("Test2").put(2, new ArrayList<>());
-//		allPaths.get("Test2").get(2).add(10);
-//		allPaths.get("Test2").get(2).add(3);
-//		//ifrandom
-//		allPaths.get("Test2").put(3, new ArrayList<>());
-//		allPaths.get("Test2").get(3).add(4);
-//		//ifempty
-//		allPaths.get("Test2").put(10, new ArrayList<>());
-//		allPaths.get("Test2").get(10).add(11);
-//		allPaths.get("Test2").get(10).add(13);
-//		//jdklsfalkaf;ds
-//		allPaths.get("Test2").put(13, new ArrayList<>());
-//		allPaths.get("Test2").get(13).add(14);
-//		allPaths.get("Test2").get(13).add(16);
-//
-//
-//
-//
-//		allPaths.put("Test1", new HashMap<>());
-//		allPaths.get("Test1").put(0, new ArrayList<>());
-//		allPaths.get("Test1").get(0).add(1);
-//		allPaths.get("Test1").get(0).add(5);
-//		//if hungry
-//		allPaths.get("Test1").put(1, new ArrayList<>());
-//		allPaths.get("Test1").get(1).add(2);
-//		allPaths.get("Test1").get(1).add(12);
-//		//ifempty
-//		allPaths.get("Test1").put(2, new ArrayList<>());
-//		allPaths.get("Test1").get(2).add(10);
-//		allPaths.get("Test1").get(2).add(3);
-//		//ifrandom
-//		allPaths.get("Test1").put(5, new ArrayList<>());
-//		allPaths.get("Test1").get(5).add(6);
-//		allPaths.get("Test1").get(5).add(8);
-//	}
-//
-//
-//	public static Set<String> testedCommands = new HashSet<>();
-
+	// returns true if the input is an integer. Otherwise, returns false.
 	private boolean isInt(String integer) {
+
 		try {
 			Integer.parseInt(integer);
 			return true;
@@ -112,7 +26,9 @@ public class Interpreter implements CritterInterpreter {
 		}
 	}
 
+	//go command
 	public int go(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if(currBehavior.get(1).charAt(0) == '+' && isInt(currBehavior.get(1).substring(1))) {
 			currStep += Integer.parseInt(currBehavior.get(1).substring(1)) - 1;
 		} else if (currBehavior.get(1).charAt(0) == '-'&& isInt(currBehavior.get(1).substring(1))) {
@@ -122,13 +38,16 @@ public class Interpreter implements CritterInterpreter {
 		} else if (isInt(currBehavior.get(1))) {
 			currStep = Integer.parseInt(currBehavior.get(1)) - 1;
 		}
+		//if this is an invalid go command
 		else {
 			System.err.println("Not valid go");
 			return -1;
 		}
 		return currStep;
 	}
+	//ifrandom command
 	public int ifrandom(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if(c.ifRandom()) {
 			if (currBehavior.get(1).charAt(0) == 'r' && isInt(currBehavior.get(1).substring(1))) {
 				currStep = c.getReg(Integer.parseInt(currBehavior.get(1).substring(1))) - 1;
@@ -136,6 +55,7 @@ public class Interpreter implements CritterInterpreter {
 			else if (isInt(currBehavior.get(1))) {
 				currStep = Integer.parseInt(currBehavior.get(1)) - 1;
 			}
+			//if this is an invalid ifrandom command
 			else {
 				return -1;
 			}
@@ -145,7 +65,10 @@ public class Interpreter implements CritterInterpreter {
 		}
 		return currStep;
 	}
+
+	//ifstarving command
 	public int ifstarving(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if(c.getHungerLevel() == Critter.HungerLevel.STARVING) {
 			if (currBehavior.get(1).charAt(0) == 'r'&&isInt(currBehavior.get(1).substring(1))) {
 				currStep = c.getReg(Integer.parseInt(currBehavior.get(1).substring(1))) - 1;
@@ -153,6 +76,7 @@ public class Interpreter implements CritterInterpreter {
 			else if (isInt(currBehavior.get(1))) {
 				currStep = Integer.parseInt(currBehavior.get(1)) - 1;
 			}
+			//if the ifstarving inputs are wrong
 			else {
 				return -1;
 			}
@@ -162,7 +86,10 @@ public class Interpreter implements CritterInterpreter {
 		}
 		return currStep;
 	}
+
+	//isempty command
 	public int ifempty(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if(isInt(currBehavior.get(1))&&c.getCellContent(Integer.parseInt(currBehavior.get(1))) == 0) {
 			if (currBehavior.get(2).charAt(0) == 'r'&&isInt(currBehavior.get(2).substring(1))) {
 				currStep = c.getReg(Integer.parseInt(currBehavior.get(2).substring(1))) - 1;
@@ -170,10 +97,57 @@ public class Interpreter implements CritterInterpreter {
 			else if (isInt(currBehavior.get(2))) {
 				currStep = Integer.parseInt(currBehavior.get(2)) - 1;
 			}
+			// if the inputs for ifempty are not valid
 			else {
 				return -1;
 			}
 		}
+		//if input is invalid
+		else if (!isInt(currBehavior.get(1))) {
+			return -1;
+		}
+		else {
+			currStep += 1;
+		}
+		return currStep;
+	}
+//ifhungry command
+	public int ifhungry(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
+		if(c.getHungerLevel() == Critter.HungerLevel.HUNGRY || c.getHungerLevel() == Critter.HungerLevel.STARVING) {
+			if (currBehavior.get(1).charAt(0) == 'r'&&isInt(currBehavior.get(1).substring(1))) {
+				currStep = c.getReg(Integer.parseInt(currBehavior.get(1).substring(1))) - 1;
+			}
+			else if (isInt(currBehavior.get(1))) {
+				currStep = Integer.parseInt(currBehavior.get(1)) - 1;
+			}
+			//if input is invalid
+			else {
+				return -1;
+			}
+		}
+		else {
+			currStep += 1;
+		}
+		return currStep;
+	}
+
+	//ifally command
+	public int ifally(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
+		if(isInt(currBehavior.get(1))&&c.getCellContent(Integer.parseInt(currBehavior.get(1))) == 3) {
+			if (currBehavior.get(2).charAt(0) == 'r'&&isInt(currBehavior.get(2).substring(1))) {
+				currStep = c.getReg(Integer.parseInt(currBehavior.get(2).substring(1))) - 1;
+			}
+			else if (isInt(currBehavior.get(2))) {
+				currStep = Integer.parseInt(currBehavior.get(2)) - 1;
+			}
+			// if the input is invalid
+			else {
+				return -1;
+			}
+		}
+		//if the input is invalid
 		else if (!isInt(currBehavior.get(1))) {
 			return -1;
 		}
@@ -183,44 +157,9 @@ public class Interpreter implements CritterInterpreter {
 		return currStep;
 	}
 
-	public int ifhungry(Critter c, ArrayList<String> currBehavior, int currStep) {
-		if(c.getHungerLevel() == Critter.HungerLevel.HUNGRY || c.getHungerLevel() == Critter.HungerLevel.STARVING) {
-			if (currBehavior.get(1).charAt(0) == 'r'&&isInt(currBehavior.get(1).substring(1))) {
-				currStep = c.getReg(Integer.parseInt(currBehavior.get(1).substring(1))) - 1;
-			}
-			else if (isInt(currBehavior.get(1))) {
-				currStep = Integer.parseInt(currBehavior.get(1)) - 1;
-			}
-			else {
-				return -1;
-			}
-		}
-		else {
-			currStep += 1;
-		}
-		return currStep;
-	}
-	public int ifally(Critter c, ArrayList<String> currBehavior, int currStep) {
-		if(isInt(currBehavior.get(1))&&c.getCellContent(Integer.parseInt(currBehavior.get(1))) == 3) {
-			if (currBehavior.get(2).charAt(0) == 'r'&&isInt(currBehavior.get(2).substring(1))) {
-				currStep = c.getReg(Integer.parseInt(currBehavior.get(2).substring(1))) - 1;
-			}
-			else if (isInt(currBehavior.get(2))) {
-				currStep = Integer.parseInt(currBehavior.get(2)) - 1;
-			}
-			else {
-				return -1;
-			}
-		}
-		else if (!isInt(currBehavior.get(1))) {
-			return -1;
-		}
-		else {
-			currStep += 1;
-		}
-		return currStep;
-	}
+	//ifenemy command
 	public int ifenemy(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if(isInt(currBehavior.get(1)) && c.getCellContent(Integer.parseInt(currBehavior.get(1))) == 2) {
 			if (currBehavior.get(2).charAt(0) == 'r'&&isInt(currBehavior.get(2).substring(1))) {
 				currStep = c.getReg(Integer.parseInt(currBehavior.get(2).substring(1))) - 1;
@@ -228,10 +167,12 @@ public class Interpreter implements CritterInterpreter {
 			else if (isInt(currBehavior.get(2))) {
 				currStep = Integer.parseInt(currBehavior.get(2)) - 1;
 			}
+			//if the input is invalid
 			else {
 				return -1;
 			}
 		}
+		//if the input is invalid
 		else if (!isInt(currBehavior.get(1))) {
 			return -1;
 		}
@@ -240,7 +181,10 @@ public class Interpreter implements CritterInterpreter {
 		}
 		return currStep;
 	}
+
+	//ifwall command
 	public int ifwall(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if(isInt(currBehavior.get(1))&&c.getCellContent(Integer.parseInt(currBehavior.get(1))) == 1) {
 			if (isInt(currBehavior.get(2).substring(1))&&currBehavior.get(2).charAt(0) == 'r') {
 				currStep = c.getReg(Integer.parseInt(currBehavior.get(2).substring(1))) - 1;
@@ -248,10 +192,12 @@ public class Interpreter implements CritterInterpreter {
 			else if (isInt(currBehavior.get(2))) {
 				currStep = Integer.parseInt(currBehavior.get(2)) - 1;
 			}
+			//invalid input
 			else {
 				return -1;
 			}
 		}
+		//invalid input
 		else if (!isInt(currBehavior.get(1))) {
 			return -1;
 		}
@@ -260,7 +206,10 @@ public class Interpreter implements CritterInterpreter {
 		}
 		return currStep;
 	}
+
+	//ifangle command
 	public int ifangle(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if(isInt(currBehavior.get(1))&&isInt(currBehavior.get(2))&&c.getOffAngle(Integer.parseInt(currBehavior.get(1))) == Integer.parseInt(currBehavior.get(2))) {
 			if (currBehavior.get(3).charAt(0) == 'r'&&isInt(currBehavior.get(3).substring(1))) {
 				currStep = c.getReg(Integer.parseInt(currBehavior.get(3).substring(1))) - 1;
@@ -268,10 +217,12 @@ public class Interpreter implements CritterInterpreter {
 			else if (isInt(currBehavior.get(3))) {
 				currStep = Integer.parseInt(currBehavior.get(3)) - 1;
 			}
+			//if the input is invalid
 			else {
 				return -1;
 			}
 		}
+		//if input is invalid
 		else if (!isInt(currBehavior.get(1))|| !isInt(currBehavior.get(2))) {
 			return -1;
 		}
@@ -280,7 +231,10 @@ public class Interpreter implements CritterInterpreter {
 		}
 		return currStep;
 	}
+
+	//iflt command
 	public int iflt(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if (isInt(currBehavior.get(1).substring(1))&&isInt(currBehavior.get(2).substring(1))&&c.getReg(Integer.parseInt(currBehavior.get(1).substring(1))) < c.getReg(Integer.parseInt(currBehavior.get(2).substring(1)))) {
 			if (currBehavior.get(3).charAt(0) == 'r'&&isInt(currBehavior.get(3).substring(1))) {
 				currStep = c.getReg(Integer.parseInt(currBehavior.get(3).substring(1))) - 1;
@@ -288,10 +242,13 @@ public class Interpreter implements CritterInterpreter {
 			else if (isInt(currBehavior.get(3))) {
 				currStep = Integer.parseInt(currBehavior.get(3)) - 1;
 			}
+
+			//if input is invalid
 			else {
 				return -1;
 			}
 		}
+		//if input is invalid
 		else if (!isInt(currBehavior.get(1).substring(1))|| !isInt(currBehavior.get(2).substring(1))) {
 			return -1;
 		}
@@ -301,7 +258,9 @@ public class Interpreter implements CritterInterpreter {
 		return currStep;
 	}
 
+	// ifeq behavior
 	public int ifeq(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if (isInt(currBehavior.get(1).substring(1))&&isInt(currBehavior.get(2).substring(1)) &&c.getReg(Integer.parseInt(currBehavior.get(1).substring(1))) == c.getReg(Integer.parseInt(currBehavior.get(2).substring(1)))) {
 			if (currBehavior.get(3).charAt(0) == 'r'&&isInt(currBehavior.get(3).substring(1))) {
 				currStep = c.getReg(Integer.parseInt(currBehavior.get(3).substring(1))) - 1;
@@ -309,10 +268,12 @@ public class Interpreter implements CritterInterpreter {
 			else if (isInt(currBehavior.get(3))) {
 				currStep = Integer.parseInt(currBehavior.get(3)) - 1;
 			}
+			// if the inputs are invalid
 			else {
 				return -1;
 			}
 		}
+		// if the input is invalid
 		else if (!isInt(currBehavior.get(1).substring(1))|| !isInt(currBehavior.get(2).substring(1))) {
 			return -1;
 		}
@@ -322,7 +283,9 @@ public class Interpreter implements CritterInterpreter {
 		return currStep;
 	}
 
+	//ifgt function
 	public int ifgt(Critter c, ArrayList<String> currBehavior, int currStep) {
+		// checking the different possibilities for the input and updating currStep
 		if (isInt(currBehavior.get(1).substring(1))&&isInt(currBehavior.get(2).substring(1)) && c.getReg(Integer.parseInt(currBehavior.get(1).substring(1))) > c.getReg(Integer.parseInt(currBehavior.get(2).substring(1)))) {
 			if (currBehavior.get(3).charAt(0) == 'r'&&isInt(currBehavior.get(3).substring(1))) {
 				currStep = c.getReg(Integer.parseInt(currBehavior.get(3).substring(1))) - 1;
@@ -330,10 +293,12 @@ public class Interpreter implements CritterInterpreter {
 			else if (isInt(currBehavior.get(3))) {
 				currStep = Integer.parseInt(currBehavior.get(3)) - 1;
 			}
+			//invalid inputs
 			else {
 				return -1;
 			}
 		}
+		//invalid inputs
 		else if (!isInt(currBehavior.get(1).substring(1))|| !isInt(currBehavior.get(2).substring(1))) {
 			return -1;
 		}
@@ -346,17 +311,18 @@ public class Interpreter implements CritterInterpreter {
 
 
 	public void executeCritter(Critter c) {
-//		createTrees();
 		ArrayList<ArrayList<String>> behaviors = new ArrayList<>(c.getCode());
+		// loading in the behavior that this critter left off on
 		int currStep = c.getNextCodeLine() -1;
 		if(currStep >= behaviors.size()) {
 			System.err.println("critter instructions point to invalid line");
 			return;
 		}
+		//loading in critter behaviors
 		ArrayList<String> currBehavior = behaviors.get(currStep);
-
+		//while loops keeps running until it detects an action command
 		while(!currBehavior.get(0).equals("hop") && !currBehavior.get(0).equals("left") && !currBehavior.get(0).equals("right") && !currBehavior.get(0).equals("eat") && !currBehavior.get(0).equals("infect") ) {
-
+			//the following if statements figure out what command currStep is on and runs the respective function to update currStep.
 			if(currBehavior.get(0).equals("go") && currBehavior.size() == 2) {
 				currStep = go(c, currBehavior, currStep);
 			}
@@ -442,32 +408,17 @@ public class Interpreter implements CritterInterpreter {
 				currStep = ifgt(c, currBehavior, currStep);
 			}
 			else {
-				System.out.println(currBehavior + "in while loop");
 				System.err.println("Critter Instructions Invalid");
 				return;
 			}
-
-
-
 			if(currStep >= behaviors.size()-1 || currStep < 0) {
 				System.err.println("critter instructions point to invalid line");
 				return;
 			}
+			//updating currBehavior
 			currBehavior = behaviors.get(currStep);
 		}
-//		if (!isValidPath(allCommands, behaviors.get(behaviors.size()-1).get(0))) {
-//			System.err.println("critter instructions point to invalid line");
-//			return;
-//		}
-//		else {
-//			for (int i = 0; i < allCommands.size(); i++) {
-//				testedCommands.add(behaviors.get(allCommands.get(i)).get(0));
-//			}
-//		}
-//		if (testedCommands.size() == 14) {
-//			System.out.println("reached all commands");
-//		}
-
+		//the following if statements run the action command
 		if(currBehavior.get(0).equals("hop")) {
 			c.hop();
 		}
@@ -499,28 +450,27 @@ public class Interpreter implements CritterInterpreter {
 			}
 		}
 		else {
-			System.out.println(currBehavior + "Hello");
 			System.err.println("Critter Instructions Invalid");
 			return;
 		}
+		//updating the last behavior that this critter has run
 		c.setNextCodeLine(currStep+2);
-
-
 	}
 
 	public CritterSpecies loadSpecies(String filename) throws IOException {
-		// obviously, your code should do something
 		ArrayList<ArrayList<String>> critterBehavior = new ArrayList<>();
 		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		//reading in name
 		String name = reader.readLine();
+
+		//reading in input file and saving them as behaviors in the following format: [string behavior, input1, input2, ..., inputN]
 		String currLine = reader.readLine();
 		while (!currLine.isEmpty()) {
 			critterBehavior.add(new ArrayList<>(Arrays.asList(currLine.split(" "))));
 			currLine = reader.readLine();
 		}
 		reader.close();
-//			critterBehavior.add(new ArrayList<>(Arrays.asList(name)));
+		//end of reading in behaviors
 		return new CritterSpecies(name, critterBehavior);
-
 	}
 }
